4bf1c2eed0d04086b74c9e7351f51f6972c1f553
Veronica Nixon <vsnixon@gmail.com>
2021-01-15 21:25:59 -0700

    Adds code to draw circle size dependent on magnitude value. Adds functionality to change circle size with zoom. Colors circles based on earthquake depth.

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,7 @@
 // * * *
 // 
 // 
-// * * *
-
+// *
 
 function createMarkers(response) 
 {
@@ -24,14 +23,17 @@ function createMarkers(response)
       var description = earthquake.properties.title;
       var date = new Date(earthquake.properties.time);
 
+      // calls getColor to get color associated with depth
+      color = getColor(depth);
+
       // For each feature (earthquake event), creates a circle with radius proportional to earthquake magnitude
       // and color proportional to earthquake depth. Binds a popup with the earthquake location description and magnitude
-      var circle = L.circle([lat, lon], {
-          fillOpacity: 0.75,
-          fillColor: "red",
-          color: "red",
+      circle = L.circle([lat, lon], {
+          fillOpacity: 0.5,
+          fillColor: color,
+          color: color,
           radius: symbolSize
-      }).bindPopup("<center>"+description+"<br>"+date+"</center>");
+      }).bindPopup("<center><p>Earthquake Details<hr>"+description+"<br>"+date+"</center>");
     
       // Adds the circle to the circles array
       circles.push(circle);
@@ -40,12 +42,12 @@ function createMarkers(response)
     circleLayerGroup = L.layerGroup(circles);
 
     // Calls createMap function, passing in the marker layer group.
-    createMap(circleLayerGroup);
+    createMap(circleLayerGroup, earthquake, circles);
   }
   
-  function createMap(circleLayerGroup) 
+  function createMap(circleLayerGroup, earthquake, circles) 
   {
-    // Create the tile layer that will be the background of our map
+    // Creates the tile layer that will be the background of our map
     var basemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
       maxZoom: 18,
@@ -53,28 +55,42 @@ function createMarkers(response)
       accessToken: API_KEY
     });
   
-    // Create an object to hold the basemap. This will be passed to the layer control.
+    // Creates an object to hold the basemap. This will be passed to the layer control.
     var baseMapChoices = {
       "Base map": basemap
     };
   
-    // Create an object to hold the layer group. This will be passed to the layer control.
+    // Creates an object to hold the layer group. This will be passed to the layer control.
     var overlayChoices = {
       "Circles": circleLayerGroup
     };
   
-    // Create the map object with default parameters
+    // Creates the map object with default parameters
     var map = L.map("map", {
-      center: [25, 0],
-      zoom: 3,
-      layers: [basemap, circleLayerGroup]
+        center: [25, 0],
+        zoom: 3,
+        layers: [basemap, circleLayerGroup]
+    }).on("zoomend", function() {
+        circleLayerGroup.eachLayer(function(layer) {
+            layer.setRadius(20000);
+        });
     });
-  
-    // Create a layer control object, pass in the base map and overlay choices. Add the layer control to the map.
+
+    // Creates a layer control object, passes in the base map and overlay choices. Add the layer control to the map.
     L.control.layers(baseMapChoices, overlayChoices, {
       collapsed: false
     }).addTo(map);
   }
+
+  // Declares function to select color based on depth of earthquake event
+  function getColor(depth) {
+      return depth < 0 ? "#ff3399":
+             depth < 50 ? "#d92626":
+             depth < 100 ? "#b94646":
+             depth < 150 ? "#996666":
+             depth < 500 ? "#808080":
+                       "#000000"
+  };
   
 // Read in API data, then call createMarkers function, passing API response. 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", createMarkers);
